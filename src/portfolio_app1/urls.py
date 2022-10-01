eb18afae073fda82d46f9980e5bb693610ee80e7
Pawel <pawel.lukmin@gmail.com>
2022-10-01 13:13:22 +0200

    Feat: Created url and view for project CRUD page.

--- src/portfolio_app1/urls.py
+++ src/portfolio_app1/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("projects/", views.projects, name="projects"),
     path("project/<int:pk>", views.project, name="project"),
-    path('profile/', views.profile, name="profile"),
+    path("profile/", views.profile, name="profile"),
+    # CRUD PATHS
+    path("create_project/", views.create_project, name="create_project"),
 ]
--- src/portfolio_app1/views.py
+++ src/portfolio_app1/views.py
@@ -1,26 +1,32 @@
 from django.shortcuts import render
+
 from portfolio_app1.models import Project
 
 
 def home(request):
-    return render(request, 'portfolio_app1/home.html')
+    return render(request, "portfolio_app1/home.html")
+
 
 def projects(request):
     projects = Project.objects.filter(active=True)
 
-    context = {'projects': projects}
-    return render(request, 'portfolio_app1/projects.html', context)
+    context = {"projects": projects}
+    return render(request, "portfolio_app1/projects.html", context)
+
 
 def project(request, pk):
     project = Project.objects.get(id=pk)
 
-    context = {'project': project}
-    return render(request, 'portfolio_app1/project.html', context)
+    context = {"project": project}
+    return render(request, "portfolio_app1/project.html", context)
+
 
 def profile(request):
-    return render(request, 'portfolio_app1/profile.html')
+    return render(request, "portfolio_app1/profile.html")
 
 
+# CRUD VIEWS
 
 
-
+def create_project(request):
+    return render(request, "base/post_form.html")
