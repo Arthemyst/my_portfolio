bab645c829a955f1471093a694fe02900ac67043
Pawel <pawel.lukmin@gmail.com>
2022-10-02 22:32:08 +0200

    Change: Used black and isort to format code.

--- src/portfolio_app1/filters.py
+++ src/portfolio_app1/filters.py
@@ -1,14 +1,20 @@
 from tkinter import Widget
+
 import django_filters
+from django import forms
 from django_filters import CharFilter
 
 from .models import *
 
-from django import forms
 
 class ProjectFilter(django_filters.FilterSet):
-    headline = CharFilter(field_name="headline", lookup_expr="icontains", label="Headline")
-    tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple)
+    headline = CharFilter(
+        field_name="headline", lookup_expr="icontains", label="Headline"
+    )
+    tags = django_filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple
+    )
+
     class Meta:
         model = Project
-        fields = ['headline', 'tags']
+        fields = ["headline", "tags"]
--- src/portfolio_app1/views.py
+++ src/portfolio_app1/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
-from .forms import ProjectForm
 from .filters import ProjectFilter
+from .forms import ProjectForm
 from .models import Project
 
 
